336d2d87dcfd04c03f72b9b1e7c0e870a04c7d2f
maks <it.vimax@gmail.com>
2018-09-30 23:17:59 +0300

    end lessons 1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import {render} from 'react-dom'
-import Article from './Article'
+import ArticleList from './ArticleList'
 import {articles} from "./fixtures";
 
-const article = articles[0]
-
-render(<Article article = {article} />, document.getElementById("container"))
+render(<ArticleList articles = {articles} />, document.getElementById("container"))
